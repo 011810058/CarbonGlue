5368a8caea09112c807ed3e019050c42bc80193a
Jasmeet Singh <https.jasmeet@gmail.com>
2017-04-30 14:31:33 -0700

    [Minor]Adding config value

--- lib/config/initConfig.py
+++ lib/config/initConfig.py
@@ -5,13 +5,13 @@ import os, sys
 
 class InitConfig(object):
     # Upload image will be processed and saved into .png format with same name
-    transcriptName = 'template1.jpg'
+    transcriptName = 'template1.png'
     tempDir = os.path.join(os.path.dirname(sys.modules['__main__'].__file__), "temp")
     templatesDir = os.path.join(os.path.dirname(sys.modules['__main__'].__file__), "templates")
+    templateInitials = "template"
     cropImageFolderName = "sample"
     cropFileInitials = "sample"
     contourImageName = "contoured.png"
-    template1_Order = {'Img1' : 'Semester.png', 'Img2' : 'Subject.png'}
     #Move this template sequence to json file and load json object 
     templateSequence = {'template1':{'1' : 'Semester.png', '2' : 'Subject.png'}}
     
