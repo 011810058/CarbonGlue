e122a23f61c4d83874683d681fe058293ebab3bb
Parag Vijayvergia <parag.vijayvergia@sjsu.edu>
2017-05-10 01:03:59 -0700

    Updating dbHelper and adding flask template to html_templates

--- lib/db_helper/dbHelper.py
+++ lib/db_helper/dbHelper.py
@@ -8,13 +8,21 @@ class DBHelper(initConfig.InitConfig):
     """ Performs MongoDB related functions """
 
     def __init__(self):
-        self.mongoClient = MongoClient()
+        try:
+            self.mongoClient = MongoClient()
+        except Exception as e:
+            print e.message
+            raise e
 
     def getCollection(self, collection_name):
         """ Return the PyMongo collection object based on given collection name """
-        db = self.mongoClient[self.databaseName]
-        coll = db[collection_name]
-        return coll
+        try:
+            db = self.mongoClient[self.databaseName]
+            coll = db[collection_name]
+            return coll
+        except Exception as e:
+            print e.message
+            return None
 
     def storeInDB(self, json_formate_string, collection_name = None):
         """ This method takes a string in json formate and stores it as json object in MongoDB """
@@ -28,9 +36,12 @@ class DBHelper(initConfig.InitConfig):
             selectedCollection = self.getCollection(collection_name)
             key = {self.studentID : json_formate_string.pop(self.studentID,None)}
             update_query = {self.set_string : json_formate_string}
-            documentID = selectedCollection.update_one(key,update_query, upsert=True).upserted_id
-            if documentID is None:  
-                raise "Exception MongoDB: Insert record failed..!!"
+            matched_count = selectedCollection.update_one(key,update_query, upsert=True).matched_count
+            if matched_count == 0:
+                print "New document has been inserted with Student ID : %s"% key[self.studentID]
+            elif matched_count > 0:
+                print "Document with Student ID %s has been Updated." % key[self.studentID]
+            
 
             return success
             
@@ -39,6 +50,10 @@ class DBHelper(initConfig.InitConfig):
         
     def findInDB(self, query):
         """ Find matching document in collection based on given keys values """ 
-        coll = self.getCollection(self.collection_name)
-        return coll.find_one(query)
+        try:
+            coll = self.getCollection(self.collection_name)
+            return coll.find_one(query)
+        except Exception as e:
+            print e.message
+            raise e
             
